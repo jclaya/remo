9fa23151ecf5ebbed9b207ebc26068abab05780f
jclaya <jbclaya@gmail.com>
2018-10-21 10:55:31 +0800

    commit

--- lib/ui/appointment_page.dart
+++ lib/ui/appointment_page.dart
@@ -3,42 +3,48 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:remo/Theme.dart' as Theme;
 
 class AppointmentPage extends StatelessWidget {
+  
+  final _formKey = GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) => new Scaffold(
-        body: new Container(
-            child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Image(
-                width: 150.0,
-                height: 150.0,
-                fit: BoxFit.fill,
-                image: new AssetImage('assets/img/sad.png')),
-            new Text('You have no appointments yet'),
-            new MaterialButton(
-                color: Theme.Colors.appBarGradientEnd,
-                highlightColor: Colors.transparent,
-                splashColor: Theme.Colors.loginGradientEnd,
-                //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 42.0),
-                  child: Text(
-                    "APPOINT ME",
-                    style: TextStyle(
-                        color: Theme.Colors.appBarGradientStart,
-                        fontSize: 25.0,
-                        fontFamily: "WorkSansBold"),
-                  ),
-                ),
-                onPressed: () {
+        body: Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, we want to show a Snackbar
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    )
+      );
+}
+
+/*
+onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => AppointmentPage()),
                   );
-                })
-          ],
-        )),
-      );
-}
+                })*/
